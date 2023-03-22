724bf6519ca8f23ad62258e2db657a78e6742726
unknownhacker2k7 <128623140+unknownhacker2k7@users.noreply.github.com>
2023-03-22 18:47:44 +0100

    Update shuvl.lua

--- shuvl.lua
+++ shuvl.lua
@@ -1,5 +1,3 @@
-
-
 for i,v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
 	v:Disable()
 end
@@ -10,83 +8,55 @@ for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
 		table.insert(tools, v.Name)
 	end
 end
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local window = ui.CreateLib("Blox Fruits", getgenv().ThemeOption)
 
----autofarm Level
----lv 0-10
-function CheckQuest()
-	local Lv = game.Players.LocalPlayer.Data.Level.Value
-	 if Lv == 0 or Lv <= 10 then
-	 Ms = "Bandit [Lv. 5]"
-	 NM = "Bandit"
-	 LQ = 1
-	 NQ = "BanditQuest1"
-	 CQ = CFrame.new(1062.64697265625, 16.516624450683594, 1546.55224609375)
-	 end
- end
- function TP(P)
-	 Distance = (P.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-	 if Distance < 10 then
-		 Speed = 1000
-	 elseif Distance < 170 then
-		 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = P
-		 Speed = 350
-	 elseif Distance < 1000 then
-		 Speed = 350
-	 elseif Distance >= 1000 then
-		 Speed = 300
-	 end
-	 game:GetService("TweenService"):Create(
-		 game.Players.LocalPlayer.Character.HumanoidRootPart,
-		 TweenInfo.new(Distance/Speed, Enum.EasingStyle.Linear),
-		 {CFrame = P}
-	 ):Play()
- end
- 
- spawn(function()
-	while task.wait() do
-		if _G.AutoFarm then
-			CheckQuest()
-			if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == false then
-				 TP(CQ)
-				 task.wait(0.9)
-				 game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StartQuest",NQ,LQ)
-				 elseif game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
-					 for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-						 if v.Name == Ms then
-						 TP(v.HumanoidRootPart.CFrame * CFrame.new(0,20,0))
-						 v.HumanoidRootPart.Size = Vector3.new(60,60,60)
-					 end
-				 end
-			 end
-		 end
-	 end
- end)
- spawn(function()
-    game:GetService("RunService").RenderStepped:Connect(function()
-		if _G.AutoFarm then
-		pcall(function()
-                game:GetService'VirtualUser':CaptureController()
-			    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1))
-            end)
-        end
-    end)
+local customColors = {
+    SchemeColor = Color3.fromRGB(0,255,255),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(0, 0, 0),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+local plr = game:GetService("Players").LocalPlayer
+------------------------------
+getgenv().AutoFarm = false
+getgenv().CurrentQuest = "Bandit [Lv. 5]"
+getgenv().FruitEsp = false
+getgenv().ChestEsp = false
+
+getgenv().SkillZ = false
+getgenv().SkillC = false
+getgenv().SkillX = false
+getgenv().SKillF = false
+
+getgenv().CurrentWeapon = "Combat"
+getgenv().AutoWeapon = false
+
+getgenv().FarmStopped = false
+getgenv().IsQuestStarted = false
+
+getgenv().Noclip = false
+-------------------------------
+local mainTab = window:NewTab("Main")
+local plrTab = window:NewTab("Player")
+local mainSection = mainTab:NewSection("Auto Farm")
+local plrSection = plrTab:NewSection("Player")
+local creditsTab = window:NewTab("Credits")
+
+local creditSection = creditsTab:NewSection("Credits")
+local xheptccredit = creditsTab:NewSection("xHeptc - UI Library / Main Scripter")
+local sanincredit = creditsTab:NewSection("sannin - Helper <3")
+
+plrSection:NewToggle("Noclip", "Enables Noclip", function(state)
+    getgenv().Noclip = state
 end)
 
+local toolDropdown = mainSection:NewDropdown("Weapon", "Choose your tool to use!", tools, function(weapon)
+	getgenv().CurrentWeapon = weapon
+end)
 
-
-
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-        local Window = Library.CreateLib("BloxFruits Script Test", "Ocean")
-      
-      ---Main
-        local Main = Window:NewTab("Main")
-        local MainSection = Main:NewSection("Main")
-
-
-        local toolDropdown = MainSection:NewDropdown("Weapon", "Choose your tool to use!", tools, function(weapon)
-        end)
-        
 game.Players.LocalPlayer.Backpack.DescendantAdded:Connect(function(tool)
 	local toolName = tool.Name
 	if tool:IsA("Tool") then
@@ -106,176 +76,117 @@ game.Players.LocalPlayer.Backpack.DescendantRemoving:Connect(function(tool)
 	toolDropdown:Refresh(tools)
 end)
 
-        MainSection:NewToggle("AutoFarm", "AutoFarm Test", function(state)
-            if state then
-                _G.AutoFarm = true
-            else
-                _G.AutoFarm = false
-            end
-        end)
-        
+local skillsSection = mainTab:NewSection("Skills")
 
-        
-        local FakeBeliTextBox MainSection:NewTextBox("Fake Beli", "Fake Beli", function(fakebeli)
-            game.Players.LocalPlayer.Data.Level.Value = fakebeli
-        end)
-        
+mainSection:NewToggle("Auto Farm", "Auto Farms Enemies", function(state)
+    getgenv().AutoFarm = state
+end)
 
-        ---Stats
-        local Stats = Window:NewTab("Stats")
-        local StatsSection = Stats:NewSection("Stats")
-
-        StatsSection:NewToggle("Meele", "Auto Stats", function(state)
-            if state then
-                _G.autoMeeleStats = true
-                while _G.autoMeeleStats == true do
-local args = {
-    [1] = "AddPoint",
-    [2] = "Melee",
-    [3] = 1
+local quests = {
+	"Bandit [Lv. 5]",
+	"Monkey [Lv. 14]", 
+	"Gorilla [Lv. 20]",
+	"Pirate [Lv. 35]", 
+	"Brute [Lv. 45]", 
+	"Desert Bandit [Lv. 60]", 
+	"Desert Officer [Lv. 70]", 
+	"Snow Bandit [Lv. 90]", 
+	"Snowman [Lv. 100]",
+	"Chief Petty Officer [Lv. 120]" ,
+	"Sky Bandit [Lv. 150]", 
+	"Toga Warrior [Lv. 225]", 
+	"Gladiator [Lv. 275]", 
+	"Military Soldier [Lv. 300]", 
+	"Military Spy [Lv. 330]", 
+	"God's Guard [Lv. 450]",
+	"Shanda [Lv. 475]", 
+	"Galley Pirate [Lv. 625]"
 }
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-                    task.wait(30)
-                end
-            else
-                print("Toggle Off")
+
+mainSection:NewDropdown("Mob", "Choose Your Mob To Farm", quests, function(chosenQuest)
+    getgenv().CurrentQuest = chosenQuest
+end)
+
+function enemy()
+    if game.Workspace.Enemies:FindFirstChild(getgenv().CurrentQuest) then
+        local mobs = game.Workspace.Enemies:GetChildren()
+        for i = 1, #mobs do local v = mobs[i]
+            if v.Name == getgenv().CurrentQuest and v:IsA("Model") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
+                return v
             end
-        end)
+        end
+    end
+    return game.ReplicatedStorage:FindFirstChild(getgenv().CurrentQuest)
+end
 
-        StatsSection:NewToggle("Defense", "Auto Stats", function(state)
-            if state then
-                _G.autoDefenseStats = true
-                while _G.autoDefenseStats == true do
-local args = {
-    [1] = "AddPoint",
-    [2] = "Defense",
-    [3] = 1
-}
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-                    task.wait(30)
-                end
-            else
-                print("Toggle Off")
-            end
-        end)
+skillsSection:NewToggle("Auto Skill: C", "Uses Skill C", function(state)
+    getgenv().SkillC = state
+end)
 
-        StatsSection:NewToggle("Sword", "Auto Stats", function(state)
-            if state then
-                _G.autoSword = true
-                while _G.autoSword == true do
-local args = {
-    [1] = "AddPoint",
-    [2] = "Sword",
-    [3] = 1
-}
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+skillsSection:NewToggle("Auto Skill: Z", "Uses Skill Z", function(state)
+    getgenv().SkillZ = state
+end)
 
-                    task.wait(30)
-                end
-            else
-                print("Toggle Off")
-            end
-        end)
+skillsSection:NewToggle("Auto Skill: X", "Uses Skill X", function(state)
+    getgenv().SkillX = state
+end)
 
-        StatsSection:NewToggle("Gun", "Auto Stats", function(state)
-            if state then
-                _G.autoGun = true
-                while _G.autoGun == true do
-local args = {
-    [1] = "AddPoint",
-    [2] = "Gun",
-    [3] = 1
-}
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-                    task.wait(30)
-                end
-            else
-                print("Toggle Off")
-            end
-        end)
+skillsSection:NewToggle("Auto Skill: F", "Uses Skill F", function(state)
+    getgenv().SkillF = state
+end)
 
-        StatsSection:NewToggle("Devil Fruit", "Auto Stats", function(state)
-            if state then
-                _G.autoDevilFruit = true
-                while _G.autoDevilFruit == true do
-local args = {
-    [1] = "AddPoint",
-    [2] = "Demon Fruit",
-    [3] = 1
-}
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-
-                    task.wait(30)
-                end
-            else
-                print("Toggle Off")
-            end
+game:GetService("RunService").Stepped:Connect(function()
+    if getgenv().getgenv().Noclip then
+        pcall(function()
+            plr.Character.Humanoid:ChangeState(11)
         end)
-
-        ---Teleport
-        local Teleport = Window:NewTab("Teleport")
-        local TeleportSection = Teleport:NewSection("Teleport")
-
-        TeleportSection:NewButton("Pirate Island", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1041.8861083984375, 16.273563385009766, 1424.93701171875)}):Play()
+    end
+    if getgenv().AutoFarm or getgenv().Noclip then
+        pcall(function()
+            plr.Character.Humanoid:ChangeState(11)
+	    local useTool = game.Players.LocalPlayer.Backpack[getgenv().CurrentWeapon]
+            plr.Character.Humanoid:EquipTool(useTool)
         end)
-
-        TeleportSection:NewButton("Marine Island", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-2896.6865234375, 41.488861083984375, 2009.27490234375)}):Play()
+    end
+    if getgenv().SkillX then
+        pcall(function()
+            keypress(0x58)
+            wait(1)
+            keyrelease(0x58)
         end)
-
-        TeleportSection:NewButton("Colosseum", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-1541.0882568359375, 7.389348983764648, -2987.40576171875)}):Play()
+    end
+    if getgenv().SkillC then
+        pcall(function()
+            keypress(0x43)
+            wait(1)
+            keyrelease(0x43)
         end)
-
-        TeleportSection:NewButton("Desert", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1094.3209228515625, 6.569626808166504, 4231.6357421875)}):Play()
+    end
+    if getgenv().SkillF then
+        pcall(function()
+            keypress(0x46)
+            wait(1)
+            keyrelease(0x46)
         end)
-        TeleportSection:NewButton("Fouatin City ", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(5529.7236328125, 429.35748291015625, 4245.5498046875)}):Play()
+    end
+    if getgenv().SkillZ then
+        pcall(function()
+            keypress(0x5A)
+            wait(1)
+            keyrelease(0x5A)
         end)
-        TeleportSection:NewButton("Jungle", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-1615.1883544921875, 36.85209655761719, 150.80490112304688)}):Play()
-        end)
-        TeleportSection:NewButton("Marine Fort", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-4846.14990234375, 20.652048110961914, 4393.65087890625)}):Play()
-        end)
-        TeleportSection:NewButton("Middle Town", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-705.99755859375, 7.852255344390869, 1547.5216064453125)}):Play()
-        end)
-        TeleportSection:NewButton("Prison", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(4841.84423828125, 5.651970863342285, 741.329833984375)}):Play()
-        end)
-        TeleportSection:NewButton("Pirate Village", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-1146.42919921875, 4.752060890197754, 3818.503173828125)}):Play()
-        end)
-        TeleportSection:NewButton("Sky 1", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-            tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-4967.8369140625, 717.6719970703125, -2623.84326171875)}):Play()
-        end)
-        TeleportSection:NewButton("Sky 2", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-7876.0771484375, 5545.58154296875, -381.19927978515625)}):Play()
-        end)
-        TeleportSection:NewButton("Snow", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(1100.361328125, 5.290674209594727, -1151.5418701171875)}):Play()
-        end)
-        TeleportSection:NewButton("Under Water", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(61135.29296875, 18.47164535522461, 1597.6827392578125)}):Play()
-        end)
-        TeleportSection:NewButton("Magma Village", "Teleport you there", function()
-            tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(45, Enum.EasingStyle.Linear)
-            tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(-5248.27197265625, 8.699088096618652, 8452.890625)}):Play()
-        end)
+    end
+end)
+coroutine.wrap(function()
+    while wait() do
+        if getgenv().AutoFarm and not getgenv().FarmStopped then
+            local v = enemy()
+            local vuser = game:GetService("VirtualUser")
+            vuser:CaptureController()
+            vuser:ClickButton1(Vector2.new())
+            pcall(function()
+                plr.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame - v.HumanoidRootPart.CFrame.lookVector * 2
+            end)
+        end
+    end
+end)()
